8e26c80108f98f1d3edbb17716f579ac60c7e1df
Erkan3034 <turguterkan55@gmail.com>
2025-07-27 23:19:14 +0300

    Set up Flask application with index route and template rendering

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+from flask import Flask, request, render_template
 import requests
 import os
 
 api_key = os.getenv("API_KEY")
+
 url = f"https://data.fixer.io/api/latest?access_key={api_key}"
 
-querystring = {"base":"USD"}
 
 
-response = requests.get(url, params=querystring)
+app = Flask(__name__) 
 
-print(response.json())
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
